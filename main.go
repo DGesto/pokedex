02470475191a88cbd3810138fc40842df115e08f
DGesto <dianagesto@gmail.com>
2025-06-17 23:37:40 +0200

    Help and Exit working

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ func main() {
 		scanner.Scan()
 		s := scanner.Text()
 		inputWords := cleanInput(s)
-		fmt.Printf("Your command was: %s\n", inputWords[0])
+		if val, ok := myCommands[inputWords[0]]; ok {
+			err := val.callback()
+			if err != nil {
+				fmt.Println("An error has occurred: %w", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 }
